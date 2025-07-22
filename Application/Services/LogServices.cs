95c3ac518545457bc0c9fc693812740be58e86b3
Kauã Alves <106326032+KauaAlvesSilva@users.noreply.github.com>
2025-07-22 16:52:44 -0300

    Add files via upload

---
+++ Application/Services/LogServices.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Interface;
+
+
+namespace Application.Services
+{
+    public class LogServices : ILogInterface
+    {
+        private readonly ILogRepository _logRepository;
+        public LogServices(ILogRepository logRepository)
+        {
+            _logRepository = logRepository;
+        }
+        public async Task<ParseLogResult> ProcessarLog(string filePath)
+        {
+               var resultado = await _logRepository.ParseLog(filePath);
+               return resultado;   
+        }
+    }
+}
